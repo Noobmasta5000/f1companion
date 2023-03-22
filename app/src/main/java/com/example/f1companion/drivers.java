ea6ef57a7ccd80e983619326559659e5f7ac40ef
Noobmasta5000 <evanmeloche123@gmail.com>
2023-03-21 17:06:55 -0700

    Added new activities

---
+++ app/src/main/java/com/example/f1companion/drivers.java
@@ -0,0 +1,14 @@
+package com.example.f1companion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class drivers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers);
+    }
+}
