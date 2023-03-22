8f32c7a7ff0df6ec8b7408cb9ff285fbcfef0178
zachs <zachsheppard99@gmail.com>
2023-03-22 12:18:28 -0700

    Added homepage with images

--- app/src/main/java/com/example/f1companion/login.java
+++ app/src/main/java/com/example/f1companion/login.java
@@ -2,13 +2,41 @@ package com.example.f1companion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login);
+        registerBtn = findViewById(R.id.register);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
 }
