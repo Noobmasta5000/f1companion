5d90e3e61d6e7255d7ccc627b2444081c6d3123d
zachs <zachsheppard99@gmail.com>
2023-03-23 15:09:03 -0700

    Added flows from login and register

--- app/src/main/java/com/example/f1companion/Home.java
+++ app/src/main/java/com/example/f1companion/Home.java
@@ -2,13 +2,40 @@ package com.example.f1companion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        loginBtn = findViewById(R.id.login);
+        registerBtn = findViewById(R.id.register);
+
+        // Click listener for login button
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), login.class);
+                startActivity(intent);
+            }
+        });
+
+        // Click listener for register button
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Register.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
